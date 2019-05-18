fd3b7595fd14a405b15a89c2f33370b4e15167de
Gustav Wengel <gustavwengel@gmail.com>
2019-05-18 20:33:47 +0200

    split

--- install_extra.sh
+++ install_extra.sh
@@ -1,4 +1,3 @@
-
 # Install other packages and do extra steps
 # Make a snap symlink so it will work in fedora without selinux
 # https://www.reddit.com/r/Fedora/comments/7x63ib/is_there_a_way_to_install_classic_confinement/
@@ -16,7 +15,7 @@ sudo sh get-docker.sh
 
 # Add user to docker group
 sudo groupadd docker
-sudo usermod -aG docker your-user
+sudo usermod -aG docker geewee
 
 # Snap
 snap install spotify
@@ -24,24 +23,10 @@ snap install spotify
 #Slack
 snap install slack --classic
 
-#oh-my-zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-#zsh as default
-chsh -s $(which zsh)
-
-# zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-
-#zsh syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 #Blacklist nouveau MAYBE
 # echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
 # dnf remove xorg-x11-drv-nouveau
-# THIS IS WHAT WORKED LAST systemctl disable nvidia-fallback.service
+# THIS IS WHAT WORKED LAST
+systemctl disable nvidia-fallback.service
 # SEE https://askubuntu.com/questions/1043638/nouveau-blacklisted-but-loaded
-
-# Powerlevel 9k font
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
---
+++ terminal.sh
@@ -0,0 +1,14 @@
+#oh-my-zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Powerlevel 9k font
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+#zsh as default
+chsh -s $(which zsh)
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+#zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
