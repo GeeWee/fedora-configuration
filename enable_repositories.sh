9f6e698b32abe6b80ea193b14ab4b3ea5a223098
Gustav Wengel <gustavwengel@gmail.com>
2021-02-07 13:18:52 +0100

    walkthrough of repositories

--- enable_repositories.sh
+++ enable_repositories.sh
@@ -1,9 +1,14 @@
 # Enables third party repositories
+# TODO this could probably automatically synchronize repos from /etc/yum.repos.d/
+
+sudo dnf install -y dnf-plugins-core # Might be needed for the --add-repo command
+
+FEDORA_VERSION=$(rpm -E %fedora)
 
 # rpmfusions
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"${FEDORA_VERSION}".noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_VERSION}.noarch.rpm -y
 
-# Chrome
+# Chrome, Steam and graphic drivers. See https://fedoraproject.org/wiki/Workstation/Third_Party_Software_Repositories
 dnf install fedora-workstation-repositories -y
 
 dnf config-manager --set-enabled google-chrome
@@ -17,23 +22,36 @@ dnf config-manager --add-repo https://download.opensuse.org/repositories/shells:
 # Yarn
 curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
 
+# Add git lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
+
+# Github cli
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+
+# Terraform
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+
+# K8s
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# ----------------------------- MICROSOFT THINGS ------------------------------------
 # Microsoft key
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # Add .net repositories
-sudo wget -q -O /etc/yum.repos.d/microsoft-prod.repo https://packages.microsoft.com/config/fedora/29/prod.repo
-
-# Teams
-sudo sh -c 'echo -e "[teams]\nname=teams\nbaseurl=https://packages.microsoft.com/yumrepos/ms-teams\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/teams.repo'
-sudo dnf check-updat
-
-# Add git lfs
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
+sudo wget -q -O /etc/yum.repos.d/microsoft-prod.repo https://packages.microsoft.com/config/fedora/"${FEDORA_VERSION}"/prod.repo
 
 # Powershell repos
 curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 sudo dnf check-update
-sudo dnf install compat-openssl10
 
 # Azure cli
 sudo sh -c 'echo -e "[azure-cli]
@@ -45,3 +63,6 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azu
 
 # VSCode
 sudo echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+
+# Teams
+sudo sh -c 'echo -e "[teams]\nname=teams\nbaseurl=https://packages.microsoft.com/yumrepos/ms-teams\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/teams.repo'
