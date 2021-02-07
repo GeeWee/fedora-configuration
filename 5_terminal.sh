f3af7f5d73b86cc44d458c8d8c47a3f50afe98ed
Gustav Wengel <gustavwengel@gmail.com>
2021-02-07 14:06:54 +0100

    added z to installation

--- 5_terminal.sh
+++ 5_terminal.sh
@@ -15,4 +15,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 #zsh syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# z finder
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+
 echo "MAKE SURE TO COPY OVER THE .zshrc file"
